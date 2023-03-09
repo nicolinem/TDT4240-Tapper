3939361aacb54d9fb32325327654231cf23f89e1
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2023-03-09 14:40:28 +0100

    Initial commit

---
+++ lwjgl3/src/main/kotlin/com/group4/tapper/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,16 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.group4.tapper.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.group4.tapper.Tapper
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    Lwjgl3Application(Tapper(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("Tapper")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
