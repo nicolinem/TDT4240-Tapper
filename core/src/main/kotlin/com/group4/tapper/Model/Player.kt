d5d0fc0525acf1679a11a5ff17508b2e0a0e6f97
tafrette <thomas.frette@gmail.com>
2023-03-16 12:39:16 +0100

    Implement ID for each player

--- core/src/main/kotlin/com/group4/tapper/Model/Player.kt
+++ core/src/main/kotlin/com/group4/tapper/Model/Player.kt
@@ -1,4 +1,16 @@
 package com.group4.tapper.Model
 
+import java.util.concurrent.atomic.AtomicInteger
 class Player {
+
+    val id: Int = generatePlayerId()
+        get() = field
+
+    // Increments the ID by 1 when an instance is made
+    companion object {
+        private val counter = AtomicInteger(0)
+        private fun generatePlayerId() = counter.incrementAndGet()
+    }
+
+
 }
