f49d1391154b0cbc039ca94d392bd8ab2e3a8dfb
tafrette <thomas.frette@gmail.com>
2023-03-22 10:14:50 +0100

    add variables

--- core/src/main/kotlin/com/group4/tapper/Model/Game.kt
+++ core/src/main/kotlin/com/group4/tapper/Model/Game.kt
@@ -1,12 +1,14 @@
 package com.group4.tapper.Model
 
-class Game {
+class Game( rounds: Int) {
 
-    var players: Array<Player> = emptyArray<Player>();
-    var puzzle: Puzzle = Puzzle();
-    var rounds: Int = 0;
+    var players: Array<Player> = emptyArray<Player>()
+        get() = field
 
+    var puzzle: Puzzle = Puzzle()
+        get() = field
 
+    var rounds: Int = rounds;
 
 
 }
