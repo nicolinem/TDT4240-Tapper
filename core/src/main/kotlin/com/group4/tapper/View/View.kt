502357d1e53e005386c2df8514b1b64a063a6a8b
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2023-04-16 17:43:56 +0200

    remove unused import

--- core/src/main/kotlin/com/group4/tapper/View/View.kt
+++ core/src/main/kotlin/com/group4/tapper/View/View.kt
@@ -1,22 +1,15 @@
 package com.group4.tapper.View
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.InputProcessor
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Vector3
 import ktx.app.KtxScreen
-import ktx.app.clearScreen
 import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
-import ktx.graphics.use
 
 
 abstract class View() : KtxScreen {
 
     protected val batch = SpriteBatch()
+
+
 
     override fun render(delta: Float) {
     }
@@ -26,6 +19,5 @@ abstract class View() : KtxScreen {
     }
 
     abstract fun update(dt: Float)
-    abstract fun render()
 
 }
